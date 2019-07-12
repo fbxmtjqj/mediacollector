9017fe23cdbbc76b7f762818b74ad52a1b9bdc6a
Buleok <rr2424@naver.com>
2019-07-12 19:02:15 +0900

    호로롤로ㅗ롤

--- app/src/main/java/com/youngwon/mediacollector/MainActivity.kt
+++ app/src/main/java/com/youngwon/mediacollector/MainActivity.kt
@@ -12,6 +12,7 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import android.widget.Toast
 import kotlinx.android.synthetic.main.content_home.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -41,14 +42,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         MediaView.setOnClickListener{
             startActivity(Intent(this@MainActivity,DownloadActivity::class.java))
         }
+
         //스위치버튼 클릭시 활성화로 텍스트변경
         ActiveSwitch.setOnCheckedChangeListener{ buttonView,isChecked ->
             if (isChecked){ //만약 스위치를 On시킨다면
                 ActiveText.text = "활성화"
+                Toast.makeText(this@MainActivity, "mediacollector가 작동합니다.", Toast.LENGTH_SHORT).show()
                 //비활성화버튼을 활성화로
             }
             else { //만약 스위치를 Off시킨다면
                 ActiveText.text = "비활성화"
+                Toast.makeText(this@MainActivity, "mediacollector가 비활성화됩니다.", Toast.LENGTH_SHORT).show()
                 //활성화를 비활성화로
             }
         }
@@ -62,7 +66,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             super.onBackPressed()
         }
     }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
