d728a873984fb75ab36322a5d13a01eea1edb9b7
N4using <dnjsxo516@naver.com>
2019-07-14 07:38:51 +0900

    download switch preferences 저장

--- app/src/main/java/com/youngwon/mediacollector/MainActivity.kt
+++ app/src/main/java/com/youngwon/mediacollector/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.youngwon.mediacollector
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
@@ -38,17 +39,31 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             startActivity(Intent(this@MainActivity,DownloadActivity::class.java))
         }
 
+        val settings: SharedPreferences = getSharedPreferences("dico", MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = settings.edit()
+        if(settings.getBoolean("switch", false)) {
+            ActiveText.text = "활성화"
+            ActiveSwitch.setChecked(true)
+        } else {
+            ActiveText.text = "비활성화"
+            ActiveSwitch.setChecked(false)
+        }
+
         //스위치버튼 클릭시 활성화로 텍스트변경
         ActiveSwitch.setOnCheckedChangeListener{ buttonView,isChecked ->
             if (isChecked){ //만약 스위치를 On시킨다면
                 ActiveText.text = "활성화"
                 Toast.makeText(this@MainActivity, "mediacollector가 작동합니다.", Toast.LENGTH_SHORT).show()
                 //비활성화버튼을 활성화로
+                editor.putBoolean("switch", isChecked)
+                editor.commit()
             }
             else { //만약 스위치를 Off시킨다면
                 ActiveText.text = "비활성화"
                 Toast.makeText(this@MainActivity, "mediacollector가 비활성화됩니다.", Toast.LENGTH_SHORT).show()
                 //활성화를 비활성화로
+                editor.putBoolean("switch", isChecked)
+                editor.commit()
             }
         }
     }
