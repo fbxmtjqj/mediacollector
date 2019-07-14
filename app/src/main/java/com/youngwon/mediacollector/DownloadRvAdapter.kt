b54ede2d5a9dff96c82f21a78b67692ee3170ad5
Buleok <rr2424@naver.com>
2019-07-14 13:14:00 +0900

    크롤링된 이미지 출력

--- app/src/main/java/com/youngwon/mediacollector/DownloadRvAdapter.kt
+++ app/src/main/java/com/youngwon/mediacollector/DownloadRvAdapter.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
 
 class DownloadRvAdapter(val context: Context, val linklist: ArrayList<String>):
     RecyclerView.Adapter<DownloadRvAdapter.Holder>() {
@@ -26,7 +29,10 @@ class DownloadRvAdapter(val context: Context, val linklist: ArrayList<String>):
 
     inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
         val text = itemView?.findViewById<TextView>(R.id.textView3)
+        val img = itemView?.findViewById<ImageView>(R.id.img)
         fun bind (str:String,context:Context) {
+            Glide.with(itemView.context).load(str)
+                .into(img)
             text?.text = str
         }
     }
---
+++ app/src/main/java/com/youngwon/mediacollector/Media.kt
@@ -0,0 +1,3 @@
+package com.youngwon.mediacollector
+import android.graphics.Bitmap
+data class Media (var photo:Bitmap)
