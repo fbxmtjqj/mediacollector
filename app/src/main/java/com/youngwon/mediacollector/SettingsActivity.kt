7038680ff63d06bb5083549a4df03441eaeb4ec5
Buleok <rr2424@naver.com>
2019-07-17 15:55:19 +0900

    프로그레스바 재업/setting액티비티임시

---
+++ app/src/main/java/com/youngwon/mediacollector/SettingsActivity.kt
@@ -0,0 +1,26 @@
+package com.youngwon.mediacollector
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
